f6fb4520c1c9531d77e2a82db86d324186648e85
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-04-06 13:23:57 +0200

    add const users, welcome info and modify startApp

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,37 @@ const startApp = () => {
     name: 'action',
     type: 'input',
     message: 'How can I help you?',
-  }]).then(async (answers: InquirerAnswers ) => {
-    console.log("Chosen action: " + answers.action);
+  }]).then(async (answers: InquirerAnswers) => {
+    switch (answers.action) {
+      case Action.List:
+        users.showAll();
+        break;
+      case Action.Add:
+        const user = await inquirer.prompt([{
+          name: 'name',
+          type: 'input',
+          message: 'Enter name',
+        }, {
+          name: 'age',
+          type: 'number',
+          message: 'Enter age',
+        }]);
+        users.add(user);
+        break;
+      case Action.Remove:
+        const name = await inquirer.prompt([{
+          name: 'name',
+          type: 'input',
+          message: 'Enter name',
+        }]);
+        users.remove(name.name);
+        break;
+      case Action.Quit:
+        Message.showColorized(MessageVariant.Info, "Bye bye!");
+        return;
+    }
+
     startApp();
-    if (answers.action === "quit")
-      return;
   });
 }
 
@@ -99,5 +125,16 @@ class UsersData implements User {
   }
 }
 
+const users = new UsersData();
+console.log("\n");
+console.info("???? Welcome to the UsersApp!");
+console.log("====================================");
+Message.showColorized(MessageVariant.Info, "Available actions");
+console.log("\n");
+console.log("list – show all users");
+console.log("add – add new user to the list");
+console.log("remove – remove user from the list");
+console.log("quit – quit the app");
+console.log("\n");
 
 startApp();
